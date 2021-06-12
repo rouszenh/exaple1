fc3154552e037a05e958b0c7b4db59f1920fc2cd
Kittipong thatan <aem.kiti9yin@gmail.com>
2021-06-12 11:42:35 +0700

    first commit

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title','เว็บไซค์ของฉัน')</title>
+</head>
+<body>
+<h1>
+<a href="/">welcome</a> | <a href="/about">about</a> | <a href="/contact">contact</a>
+<hr>
+@yield('content')
+</h1>
+<h4>BY Aem Peach 12/06/2564</h4>
+</body>
+</html>
